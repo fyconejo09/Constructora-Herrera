10776de5e53cf89acc26737e2cb78cf02b683120
fyconejo09 <activos@constructoraherrera.com>
2021-08-11 00:12:33 -0600

    FCM0012

--- CSS/Menu.js
+++ CSS/Menu.js
@@ -3,7 +3,19 @@ $(document).ready(main);
 var contador = 1;
 function main ()   {
     $('.menu_bar').click(function(){
-      $('nav').toggle();
+      //$('nav').toggle();
+
+      if(contador == 1){
+        $('nav').animate({
+          left:'0'
+        });
+      } else {
+
+        $('nav').animate({
+          left:'-100%'
+        });
+
+      }
 
     });
 

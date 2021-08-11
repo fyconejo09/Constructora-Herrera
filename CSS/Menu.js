ebc233a165b5740f5c018468adf305fce9309956
fyconejo09 <activos@constructoraherrera.com>
2021-08-11 00:53:37 -0600

    FCM0053

--- CSS/Menu.js
+++ CSS/Menu.js
@@ -2,23 +2,22 @@ $(document).ready(main);
 
 var contador = 1;
 
-function main (){
-    $('.menu_bar').click(function(){
-      //$('nav').toggle();
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle();
 
-      if(contador == 1){
-        $('nav').animate({
-          left:'0'
-        });
-        contador = 0;
-      } else {
-        contador = 1;
-        $('nav').animate({
-          left:'-100%'
-        });
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
 
-      }
+	});
 
-    });
-
-  };
+};
